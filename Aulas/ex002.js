c221cfe7162e97cc35712d8d54d3c16314401417
Leticia Saraiva <161881588+leticiasaraivafontenele@users.noreply.github.com>
2024-03-22 17:53:25 -0300

    começando o curso xd

---
+++ Aulas/ex002.js
@@ -0,0 +1,6 @@
+"use strict"//Tenho que delcarar variaveis!!! let, var, const
+
+let nome = "Shuya"
+console.log("Gato")
+console.log(nome)
+console.log("nome:" + nome)
---
+++ Aulas/ex003.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var noma = "Shuya"; //var: nn mt controle
+let gato = "Tipinho"//let: so no escopo que foi criado pra menos
+const anao = "anaozinho"//const: nn posso atribuir um novo valor a ela dsp
+
+gato = 3
+console.log(gato) //converao de tipo automatica
